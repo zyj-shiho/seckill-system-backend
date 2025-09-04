6a743ceb735de726d943329b058d01e62cb9d988
wangyuxuan <2624765842@qq.com>
2025-09-05 00:11:30 +0800

    feat: init seckill system backend with Spring Boot 3, Redis, RabbitMQ

---
+++ src/main/java/com/seckill/backend/SeckillSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seckill.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seckill/backend/SeckillSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seckill.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
